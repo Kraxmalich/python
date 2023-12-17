237972256ffd39d24843392d8f849a15977dbf7b
Kraxmalich <dmickevich43@gmail.com>
2023-12-18 00:14:01 +0300

    new lesson

--- GB/HelloWorld.py
+++ GB/HelloWorld.py
@@ -1,9 +1,4 @@
-def large(arr):
-    max_ = arr[0]
-    for ele in arr:
-        if ele > arr:
-            max_ = ele
-            return max_
-list1 = [1,4,8,7,5]
-result = large(list1)
-print(result)
+colors = ['red', 'green', 'blue']
+data = open('c:/python/GB/file.txt', 'r')
+data.writelines(colors)
+data.close()
